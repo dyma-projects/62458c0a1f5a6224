eef490c9d7ee6ec98c8972eb6c0ee77fc1e93344
W3fox <w3fox.contact@gmail.com>
2022-05-16 18:23:53 +0200

    Mise à jour exercice

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-exercice1-enfant',
-  templateUrl: './exercice1-enfant.component.html',
-  styleUrls: ['./exercice1-enfant.component.css']
+  selector: "app-exercice1-enfant",
+  templateUrl: "./exercice1-enfant.component.html",
+  styleUrls: ["./exercice1-enfant.component.css"],
 })
 export class Exercice1EnfantComponent implements OnInit {
+  @Input() public compteur: number;
+  @Output("changeNumber") public changeNumber = new EventEmitter();
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  add() {
+    this.compteur++;
+    this.changeNumber.emit({ value: this.compteur });
   }
 
+  less() {
+    this.compteur--;
+    this.changeNumber.emit({ value: this.compteur });
+  }
 }
--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-exercice2',
-  templateUrl: './exercice2.component.html',
-  styleUrls: ['./exercice2.component.css']
+  selector: "app-exercice2",
+  templateUrl: "./exercice2.component.html",
+  styleUrls: ["./exercice2.component.css"],
 })
 export class Exercice2Component implements OnInit {
   public valeur: string;
 
-  constructor() { }
+  @ViewChild("changeInput") public input: ElementRef<HTMLInputElement>;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  change() {
+    this.valeur = this.input.nativeElement.value;
   }
-
 }
